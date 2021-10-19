105dd469e0d94032915d85a8e907dcfaaf19221e
Cahya Eka Permana <43898558+cahyaekapermana@users.noreply.github.com>
2021-10-19 18:56:21 +0700

    .htaccess & view result

--- application/controllers/C_demo.php
+++ application/controllers/C_demo.php
@@ -12,7 +12,17 @@ class C_demo extends CI_Controller
             'title' => "Cek Kematangan"
         );
         $this->load->view('template/header', $data);
-        $this->load->view('demo/V_demo');
+        $this->load->view('demo_view/V_demo');
+        $this->load->view('template/footer');
+    }
+
+    function result()
+    {
+        $data = array(
+            'title' => "Hasil Prediksi"
+        );
+        $this->load->view('template/header', $data);
+        $this->load->view('demo_view/V_result');
         $this->load->view('template/footer');
     }
 }
---
+++ application/views/demo_view/V_result.php
@@ -0,0 +1,31 @@
+<main id="main">
+    <br><br>
+    <section id="pricing" class="pricing">
+
+        <div class="container" data-aos="fade-up">
+
+            <header class="section-header">
+                <h2>Hasil Prediksi</h2>
+                <p>Hasil prediksi sebagai berikut : </p>
+            </header>
+
+            <div class="row">
+                <div class="col">
+                    <div class="card mb-3">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><b><i>Tulis hasil prediksi disini</i></b></h5>
+                            <p class="card-text"><i>isi konten disini, misal persentase dan penjelasan lainnya</i></p>
+                            <p class="card-text"><small class="text-muted">ML #by ameliaprocoder</small></p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section><!-- End Pricing Section -->
+
+</main>
